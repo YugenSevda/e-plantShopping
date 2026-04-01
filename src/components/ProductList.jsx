dbafb096c1659ec8766a44887fca5d8cb42fdd05
YugenSevda <yugensevda@gmail.com>
2026-04-01 09:47:25 +0530

    updated ProductList with 6 plants per category

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,149 +1,95 @@
-import { useDispatch }
-from "react-redux";
-
-import { addItem }
-from "../redux/CartSlice";
-
-import { useState }
-from "react";
-
-const plants = {
-
- Indoor:[
-  {
-   id:1,
-   name:"Snake Plant",
-   price:300,
-   img:"https://i.imgur.com/6pKQZ5B.png"
-  },
-
-  {
-   id:2,
-   name:"Peace Lily",
-   price:350,
-   img:"https://i.imgur.com/PhQ9N6R.png"
-  }
- ],
-
- Medicinal:[
-
-  {
-   id:3,
-   name:"Tulsi",
-   price:150,
-   img:"https://i.imgur.com/2DhmtJ4.png"
-  },
-
-  {
-   id:4,
-   name:"Aloe Vera",
-   price:250,
-   img:"https://i.imgur.com/8zj1YyE.png"
-  }
-
- ],
-
- Flower:[
-
-  {
-   id:5,
-   name:"Rose",
-   price:200,
-   img:"https://i.imgur.com/K8bG8yW.png"
-  },
-
-  {
-   id:6,
-   name:"Mint",
-   price:120,
-   img:"https://i.imgur.com/k2E9m1B.png"
-  }
-
- ]
-
-};
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/CartSlice";
 
 function ProductList(){
 
  const dispatch = useDispatch();
 
- const [added,setAdded] =
- useState([]);
+ const [added,setAdded] = useState({});
+
+ const plants = {
+
+  Indoor:[
+   {id:1,name:"Snake Plant",price:10,img:"https://i.imgur.com/1.jpg"},
+   {id:2,name:"Peace Lily",price:12,img:"https://i.imgur.com/2.jpg"},
+   {id:3,name:"Spider Plant",price:8,img:"https://i.imgur.com/3.jpg"},
+   {id:4,name:"Aloe Vera",price:9,img:"https://i.imgur.com/4.jpg"},
+   {id:5,name:"Rubber Plant",price:15,img:"https://i.imgur.com/5.jpg"},
+   {id:6,name:"ZZ Plant",price:18,img:"https://i.imgur.com/6.jpg"}
+  ],
+
+  Medicinal:[
+   {id:7,name:"Tulsi",price:7,img:"https://i.imgur.com/7.jpg"},
+   {id:8,name:"Mint",price:6,img:"https://i.imgur.com/8.jpg"},
+   {id:9,name:"Neem",price:11,img:"https://i.imgur.com/9.jpg"},
+   {id:10,name:"Ashwagandha",price:14,img:"https://i.imgur.com/10.jpg"},
+   {id:11,name:"Brahmi",price:13,img:"https://i.imgur.com/11.jpg"},
+   {id:12,name:"Giloy",price:16,img:"https://i.imgur.com/12.jpg"}
+  ],
+
+  Flower:[
+   {id:13,name:"Rose",price:5,img:"https://i.imgur.com/13.jpg"},
+   {id:14,name:"Tulip",price:9,img:"https://i.imgur.com/14.jpg"},
+   {id:15,name:"Sunflower",price:7,img:"https://i.imgur.com/15.jpg"},
+   {id:16,name:"Lily",price:8,img:"https://i.imgur.com/16.jpg"},
+   {id:17,name:"Daisy",price:6,img:"https://i.imgur.com/17.jpg"},
+   {id:18,name:"Orchid",price:20,img:"https://i.imgur.com/18.jpg"}
+  ]
+
+ };
+
+ function handleAdd(plant){
+
+  dispatch(addItem(plant));
+
+  setAdded({
+   ...added,
+   [plant.id]:true
+  });
+
+ }
 
  return(
 
   <div>
 
-   <h1>Plants 🌿</h1>
+   {Object.entries(plants).map(([category,list])=>(
 
-   {
+    <div key={category}>
 
-    Object.entries(plants)
-    .map(([category,items])=>(
+     <h2>{category} Plants</h2>
 
-     <div key={category}>
+     <div style={{display:"flex",gap:"20px"}}>
 
-      <h2>
+      {list.map((plant)=>(
 
-       {category}
+       <div key={plant.id}>
 
-      </h2>
+        <img src={plant.img} width="120"/>
 
-      {
+        <h4>{plant.name}</h4>
 
-       items.map(p=>(
+        <p>${plant.price}</p>
 
-        <div key={p.id}>
+        <button
+         disabled={added[plant.id]}
+         onClick={()=>handleAdd(plant)}
+        >
 
-         <img
-         src={p.img}
-         width="120"
-         />
+         {added[plant.id] ? "Added to Cart" : "Add to Cart"}
 
-         <h3>
+        </button>
 
-          {p.name}
+       </div>
 
-         </h3>
-
-         <p>
-
-          ₹{p.price}
-
-         </p>
-
-         <button
-
-         disabled={added.includes(p.id)}
-
-         onClick={()=>{
-
-          dispatch(addItem(p));
-
-          setAdded([
-           ...added,
-           p.id
-          ]);
-
-         }}
-
-         >
-
-          Add to Cart
-
-         </button>
-
-        </div>
-
-       ))
-
-      }
+      ))}
 
      </div>
 
-    ))
+    </div>
 
-   }
+   ))}
 
   </div>
 
